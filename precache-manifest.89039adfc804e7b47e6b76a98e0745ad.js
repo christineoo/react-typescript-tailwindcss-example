46575852dc7047739c9ebcaa03fa8b01ef089e74
Christine Oo <ootjinvoon@gmail.com>
2019-10-09 23:31:40 +0900

    Updates

---
+++ precache-manifest.89039adfc804e7b47e6b76a98e0745ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bb621d011fe0fdd7c2ec98bd9365a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55079f6940641d6668d7",
+    "url": "/static/css/main.8f779699.chunk.css"
+  },
+  {
+    "revision": "df7774c29156fabf98fa",
+    "url": "/static/js/2.5126fdae.chunk.js"
+  },
+  {
+    "revision": "55079f6940641d6668d7",
+    "url": "/static/js/main.5fa13939.chunk.js"
+  },
+  {
+    "revision": "f5b7baf3c69c89118a24",
+    "url": "/static/js/runtime-main.9b038faa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "760a3ee424f40cd7b8da50ac4818592f",
+    "url": "/static/media/tailwindcss-logo.760a3ee4.svg"
+  },
+  {
+    "revision": "2239d0f16cb703540c205dd8cb0c0cb7",
+    "url": "/static/media/typescript-logo.2239d0f1.svg"
+  }
+]);
