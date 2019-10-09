d345b52e15e14d43b175e8bcdb2e8a5379915ce2
Christine Oo <ootjinvoon@gmail.com>
2019-10-09 23:51:45 +0900

    Updates

---
+++ precache-manifest.a0f79573265daeb70ec727127b697c30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3fc38dc2e2f9d3ef3f88743eecad71",
+    "url": "/react-typescript-tailwindcss-example/index.html"
+  },
+  {
+    "revision": "98996baf66d7b97a269a",
+    "url": "/react-typescript-tailwindcss-example/static/css/main.8f779699.chunk.css"
+  },
+  {
+    "revision": "df7774c29156fabf98fa",
+    "url": "/react-typescript-tailwindcss-example/static/js/2.5126fdae.chunk.js"
+  },
+  {
+    "revision": "98996baf66d7b97a269a",
+    "url": "/react-typescript-tailwindcss-example/static/js/main.418318c5.chunk.js"
+  },
+  {
+    "revision": "392214b5b7193a048bab",
+    "url": "/react-typescript-tailwindcss-example/static/js/runtime-main.57cb6bb6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-typescript-tailwindcss-example/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "760a3ee424f40cd7b8da50ac4818592f",
+    "url": "/react-typescript-tailwindcss-example/static/media/tailwindcss-logo.760a3ee4.svg"
+  },
+  {
+    "revision": "2239d0f16cb703540c205dd8cb0c0cb7",
+    "url": "/react-typescript-tailwindcss-example/static/media/typescript-logo.2239d0f1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89039adfc804e7b47e6b76a98e0745ad.js"
+  "/react-typescript-tailwindcss-example/precache-manifest.a0f79573265daeb70ec727127b697c30.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-typescript-tailwindcss-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
